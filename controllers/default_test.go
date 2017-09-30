4ab6e4fda1f318aec52829efa083ce024eba1abc
Michael Bond <MichaelRBond@users.noreply.github.com>
2017-09-30 06:48:04 -0400

    Test controllers.DefaultHandler()

---
+++ controllers/default_test.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	resp := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	DefaultHandler(resp, req)
+
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fail()
+	}
+	resultStr := string(result)
+	assert.Equal(resp.Code, 200)
+	assert.NotContains(resultStr, "error")
+	assert.Contains(resultStr, "default handler")
+}
