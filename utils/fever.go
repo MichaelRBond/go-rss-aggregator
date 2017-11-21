53f364030661e6d74bede1fa61d9f06adfc69b16
Michael Bond <MichaelRBond@users.noreply.github.com>
2017-11-21 06:47:20 -0500

    fix types used in rss utils

--- utils/fever.go
+++ utils/fever.go
@@ -5,7 +5,7 @@ import "github.com/michaelrbond/go-rss-aggregator/types"
 // Utility functions for the FeverAPI
 
 // FeverGroupsFromRSSGroups converts []RSSGroups to []FeverGroups
-func FeverGroupsFromRSSGroups(groups []types.RSSGroups) []types.FeverGroups {
+func FeverGroupsFromRSSGroups(groups []types.RSSGroup) []types.FeverGroup {
 	var feverGroups []types.FeverGroup
 
 	return feverGroups
